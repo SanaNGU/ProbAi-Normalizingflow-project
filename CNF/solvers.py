093306f31da9b3da4e2c89a60231555d0659301f
sanaNGU <sana.sabah.sabry.al-azzawi@ltu.se>
2022-09-26 21:35:46 +0200

    CNF

---
+++ CNF/solvers.py
@@ -0,0 +1,50 @@
+import abc
+import torch
+#from .event_handling import find_event
+from misc import _handle_unused_kwargs
+
+
+class AdaptiveStepsizeODESolver(metaclass=abc.ABCMeta):
+    def __init__(self, dtype, y0, norm, **unused_kwargs):
+        _handle_unused_kwargs(self, unused_kwargs)
+        del unused_kwargs
+
+        self.y0 = y0
+        self.dtype = dtype
+
+        self.norm = norm
+
+    def _before_integrate(self, t):
+        pass
+
+    @abc.abstractmethod
+    def _advance(self, next_t):
+        raise NotImplementedError
+
+    @classmethod
+    def valid_callbacks(cls):
+        return set()
+
+    def integrate(self, t):
+        solution = torch.empty(len(t), *self.y0.shape, dtype=self.y0.dtype, device=self.y0.device)
+        solution[0] = self.y0
+        t = t.to(self.dtype)
+        self._before_integrate(t)
+        for i in range(1, len(t)):
+            solution[i] = self._advance(t[i])
+        return solution
+
+
+class AdaptiveStepsizeEventODESolver(AdaptiveStepsizeODESolver, metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def _advance_until_event(self, event_fn):
+        raise NotImplementedError
+
+    def integrate_until_event(self, t0, event_fn):
+        t0 = t0.to(self.y0.device, self.dtype)
+        self._before_integrate(t0.reshape(-1))
+        event_time, y1 = self._advance_until_event(event_fn)
+        solution = torch.stack([self.y0, y1], dim=0)
+        return event_time, solution
+
